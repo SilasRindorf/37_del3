384600b232db0fba641f3ac13cb66e8f2296da13
Silas Rindorf <s195474@student.dtu.dk>
2019-11-15 09:17:41 +0100

    Changed option to buy to force buy

--- src/main/java/Entities/Tiles/Street.java
+++ src/main/java/Entities/Tiles/Street.java
@@ -6,12 +6,10 @@ import gui_main.GUI;
 public class Street{
     public void encounter(GUI gui, GUI_Street street, GUI_Player player){
         if (street.getOwnerName() == null) {
-            if (gui.getUserLeftButtonPressed(" do you want to buy this property", "Yes", "No")) {
                 street.setOwnerName(player.getName());
                 street.setBorder(player.getPrimaryColor());
                 player.setBalance(player.getBalance()-100);
             }
-        }
         else {
             System.out.println(street.getSubText());
             //player.setBalance(street.getRent());
