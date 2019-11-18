2385afc268881e1da05c99a308b59cdaf2823169
Silas Rindorf <s195474@student.dtu.dk>
2019-11-18 12:46:10 +0100

    Fixed spelling error

--- src/main/java/Logic/Creator.java
+++ src/main/java/Logic/Creator.java
@@ -2,9 +2,12 @@ package Logic;
 
 import gui_fields.GUI_Car;
 import gui_fields.GUI_Player;
+import java.util.Random;
+import java.awt.*;
 
 public class Creator {
     public GUI_Car createCar(int maxCurrentPlayers, GUI_Player[] players){
+
         GUI_Car car = new GUI_Car();
         for (int j = 0; j < maxCurrentPlayers; j++) {
             if (car.getPrimaryColor() == players[j].getCar().getPrimaryColor())
