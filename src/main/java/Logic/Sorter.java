0ee8fb9d7215c5a3dafe0c2a476e65a8a9c9e7f8
Morten Kruuse <s183681@student.dtu.dk>
2019-11-26 18:25:12 +0100

    Created sorter

--- src/main/java/Logic/Sorter.java
+++ src/main/java/Logic/Sorter.java
@@ -7,40 +7,57 @@ import Logic.Tiles.Logic_Street;
 
 public class Sorter {
     private PlayerList playerList;
+
     public void findLogicField(Logic_Field[] logic_field, int playerID, int playerPosition) {
         //Start
         if (logic_field[playerPosition].getFieldID() == 1) {
         }
         //Chance
-        else if (logic_field[playerPosition].getFieldID() == 2){
+        else if (logic_field[playerPosition].getFieldID() == 2) {
         }
         //JailVisit
-        else if (logic_field[playerPosition].getFieldID() == 3){
+        else if (logic_field[playerPosition].getFieldID() == 3) {
         }
         //Parking
-        else if (logic_field[playerPosition].getFieldID() == 4){
+        else if (logic_field[playerPosition].getFieldID() == 4) {
         }
         //goToJail
-        else if (logic_field[playerPosition].getFieldID() == 5){
+        else if (logic_field[playerPosition].getFieldID() == 5) {
         }
         //Street
-        else if (logic_field[playerPosition].getFieldID() == 6){
+        else if (logic_field[playerPosition].getFieldID() == 6) {
             Logic_Street logicStreet = (Logic_Street) logic_field[playerPosition];
-            if (logicStreet.isHasOwner()){
+            if (logicStreet.isHasOwner()) {
                 int temp = 1;
-                if((playerPosition + 1) > logic_field.length)
-                playerList.getPlayer(playerID).setBalance(playerList.getPlayer(playerID).getBalance()-logicStreet.getRent()*temp);
-                playerList.getPlayer(logicStreet.getOwnerID()).setBalance(playerList.getPlayer(logicStreet.getOwnerID()).getBalance()+logicStreet.getRent()*temp);
-            }
-                
-            
-        }
 
+                if ((playerPosition + 1) < logic_field.length && logic_field[playerPosition + 1].getFieldID() == 6) {
+                    Logic_Street tempStreet = (Logic_Street) logic_field[playerPosition + 1];
+                    if (logicStreet.getOwnerID() == tempStreet.getOwnerID()) {
+                        temp = 2;
+                    }
+                } else if (logic_field[playerPosition - 1].getFieldID() == 6) {
+                    Logic_Street tempStreet = (Logic_Street) logic_field[playerPosition - 1];
+                    if (logicStreet.getOwnerID() == tempStreet.getOwnerID()) {
+                        temp = 2;
+                    }
+
+                    playerList.getPlayer(playerID).setBalance(playerList.getPlayer(playerID).getBalance() - logicStreet.getRent() * temp);
+                    playerList.getPlayer(logicStreet.getOwnerID()).setBalance(playerList.getPlayer(logicStreet.getOwnerID()).getBalance() + logicStreet.getRent() * temp);
+                }
+
+
+
+            }
+            else {
+                playerList.getPlayer(playerID).setBalance(playerList.getPlayer(playerID).getBalance() - logicStreet.getPropertyPrice());
+                logicStreet.setHasOwner(true, playerID);
+            }
+
+        }
     }
+
     public void setPlayerList(PlayerList playerList) {
         this.playerList = playerList;
     }
-    
+
 }
-
-
