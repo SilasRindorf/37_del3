9657e67595d46c807d76065d66b720b8e50b3dc2
Morten Kruuse <s183681@student.dtu.dk>
2019-11-28 12:33:39 +0100

    Added a winner

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -11,7 +11,7 @@ import gui_main.GUI;
 import java.awt.*;
 
 public class Controller {
-    private final Color BOARDCOLOR = new Color (245,245,220);
+    private final Color BOARDCOLOR = new Color(245, 245, 220);
 
     public void playGame() throws Exception {
         //Setup for game
@@ -25,8 +25,8 @@ public class Controller {
 
 
         //Choose language
-        String language = gui.getUserSelection("Choose language","English","Danish");
-        if (!language.equalsIgnoreCase("english")){
+        String language = gui.getUserSelection("Choose language", "English", "Danish");
+        if (!language.equalsIgnoreCase("english")) {
             gui.close();
             board.createBoard(language);
             fields = board.getGui_fields();
@@ -38,13 +38,13 @@ public class Controller {
         //Get number of players
         int playerCount = 0;
         while (playerCount > 4 || playerCount < 2)
-            playerCount = gui.getUserInteger(rf.fileToStringArray()[rf.findFirstWord("PLAYERCOUNT")+1],2,4);
+            playerCount = gui.getUserInteger(rf.fileToStringArray()[rf.findFirstWord("PLAYERCOUNT") + 1], 2, 4);
         PlayerList playerList = new PlayerList(playerCount);
         ControllerPlayer pc = new ControllerPlayer(gui);
 
         //Give players a name
         for (int i = 0; i < playerCount; i++) {
-            String name = gui.getUserString("Input player " + playerList.getPlayer(i).getName() +  "'s name");
+            String name = gui.getUserString("Input player " + playerList.getPlayer(i).getName() + "'s name");
             playerList.getPlayer(i).setName("Player " + playerList.getPlayer(i).getName() + " " + name);
         }
         pc.addPlayers(playerList);
@@ -63,26 +63,33 @@ public class Controller {
         //Game loop
         while (true) {
             //Dice throw and move player
-            for (int i=0;i < playerList.getPlayers().length; i++) {
+            for (int i = 0; i < playerList.getPlayers().length; i++) {
 
                 gui.showMessage(playerList.getPlayer(i).getName() + " is rolling the dices!");
                 dice.rollDice();
                 gui.setDie(dice.getEyes());
 
-                gui.getFields()[cm.getMovement().getCarPosition(i)].setCar(pc.getPlayers()[i],false);
-                cm.moveCar(i,dice.getEyes());
+                gui.getFields()[cm.getMovement().getCarPosition(i)].setCar(pc.getPlayers()[i], false);
+                cm.moveCar(i, dice.getEyes());
 
-                gui.getFields()[cm.getMovement().getCarPosition(i)].setCar(pc.getPlayers()[i],true);
-                sorter.findLogicField(board.getLogic_fields(),i,cm.getMovement().getCarPosition(i));
+                gui.getFields()[cm.getMovement().getCarPosition(i)].setCar(pc.getPlayers()[i], true);
+                sorter.findLogicField(board.getLogic_fields(), i, cm.getMovement().getCarPosition(i));
 
 
                 gui.showMessage(gui.getFields()[cm.getMovement().getCarPosition(playerList.getPlayer(i).getId())].getDescription());
                 //estateHasOwner.setLogicStreet(board.getLogic_fields());
                 if (cm.getMovement().isPassedStart())
                     playerList.getPlayer(i).setBalance(playerList.getPlayer(i).getBalance() + 200);
-                pc.updatePlayer(playerList,i);
-                if (playerList.getPlayer(i).getBalance() <= 0){
+                pc.updatePlayer(playerList, i);
+                if (playerList.getPlayer(i).getBalance() <= 0) {
                     gui.showMessage(playerList.getPlayer(i).getName() + " has no money left and lost");
+                    int winningPlayerID = 0;
+                    for (int j = 0; j < playerList.getPlayers().length; j++) {
+                        if (playerList.getPlayer(winningPlayerID).getBalance() < playerList.getPlayer(j).getBalance()) {
+                            winningPlayerID = j;
+                        }
+                    }
+                    gui.showMessage(playerList.getPlayer(winningPlayerID).getName() + "has the most money and has won");
                     return;
                 }
             }
